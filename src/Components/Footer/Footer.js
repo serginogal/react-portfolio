377ffc64a6449d71da5d2daa6d6c8a697ae5a03e
obviouswhy <sergilockerz@gmail.com>
2021-03-08 18:39:39 +0900

    better syntax in scroll links navbar + added scroll links in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,17 @@
 import './Footer.css'
 import React from 'react'
 import Logo from '../Logo/Logo'
+import {Link} from 'react-scroll'
 
 const Footer = ({ }) => {
   return (
     <div className="footer-wrapper">
     <Logo color={'#fff'} />
     <div className="footer-links">
-      <a href="#" className="main-link">Home</a>
-      <a href="#">Technologies</a>
-      <a href="#">Career</a>
-      <a href="#">Education</a>
+      <Link className="main-link" to='landing-page' smooth duration={500}>Home</Link>
+      <Link to='skills' smooth offset={-50} duration={500}>Technologies</Link>
+      <Link to='career' smooth offset={-50} duration={500}>Career</Link>
+      <Link to='education' smooth offset={-50} duration={500}>Education</Link>
     </div>
   </div>
   )
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,17 +1,17 @@
 import './NavBar.css'
 import React from 'react'
 import Logo from '../Logo/Logo'
-import {animateScroll as scroll, Link} from 'react-scroll'
+import {Link} from 'react-scroll'
 
 const NavBar = () => {
   return (
     <div className="nav floating">
       <Logo />
       <div className="links">
-        <a className="main-link"><Link to='landing-page' smooth duration={500}>Home</Link></a>
-        <a ><Link to='skills' smooth offset={-50} duration={500}>Technologies</Link></a>
-        <a ><Link to='career' smooth offset={-50} duration={500}>Career</Link></a>
-        <a ><Link to='education' smooth offset={-50} duration={500}>Education</Link></a>
+        <Link className="main-link" to='landing-page' smooth duration={500}>Home</Link>
+        <Link to='skills' smooth offset={-50} duration={500}>Technologies</Link>
+        <Link to='career' smooth offset={-50} duration={500}>Career</Link>
+        <Link to='education' smooth offset={-50} duration={500}>Education</Link>
       </div>
     </div>
   )
