9986dd5880a6a1a254ebe3d90373a76b1e977f32
obviouswhy <sergilockerz@gmail.com>
2021-03-12 18:39:15 +0900

    added sns links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -13,6 +13,20 @@ const Footer = ({ }) => {
       <Link to='career' smooth offset={-50} duration={500}>Career</Link>
       <Link to='education' smooth offset={-50} duration={500}>Education</Link>
     </div>
+    <div className='sns-wrapper'>
+      <div className='sns-img-container' onClick={() => window.location.href = 'mailto:snogal.cm@gmail.com'}>
+        <img className='sns-img' src={require('../../Assets/Images/Other/envelope.svg').default} alt='github logo' />
+      </div>
+      <div className='sns-img-container' onClick={() => window.open('https://github.com/obviouswhy/')}>
+        <img className='sns-img' src={require('../../Assets/Images/Skills/github-square.svg').default} alt='github logo' />
+      </div>
+      <div className='sns-img-container' onClick={() => window.open('https://www.linkedin.com/in/sergi-nogal-13493b13b/')}>
+        <img className='sns-img' src={require('../../Assets/Images/Other/linkedin.svg').default} alt='github logo' />
+      </div>
+      <div className='sns-img-container' onClick={() => window.open('https://www.instagram.com/__nogu/')}>
+        <img className='sns-img' src={require('../../Assets/Images/Other/instagram-square.svg').default} alt='github logo' />
+      </div>
+    </div>
   </div>
   )
 }
