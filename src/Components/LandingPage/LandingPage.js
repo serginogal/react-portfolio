b2ec72dc66f14e9e0707839ef918b34e08579ebf
obviouswhy <sergilockerz@gmail.com>
2021-03-16 14:12:14 +0900

    changed landing image + animated landing text and image

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,13 +2,23 @@ import './LandingPage.css'
 import React from 'react'
 import Button from '../Button/Button'
 import resume from '../../Assets/Sergi_Nogal_CV.pdf'
+import { motion } from "framer-motion"
 
 const LandingPage = () => {
 
   return (
       <div id='landing-page' className="landing-wrapper">
-        <div className="landing-text">
-          {/* <h1>Hi, I am a <span className="highlight">Frontend Developer</span> </h1> */}
+        <motion.div className="landing-text"
+          initial={{
+            opacity:0,
+            x: '-2em'
+          }}
+          animate={{
+            opacity: 1,
+            x: 0
+          }}
+          transition={{ delay: 0.1, ease: "easeOut", duration: 0.5 }}
+        >
           <h1>Hi! 👋<br/>
           My name is <span className="highlight">Sergi Nogal</span><br/>
           I'm a <span className="highlight">Frontend Developer 👨‍💻</span><br/>
@@ -16,10 +26,22 @@ const LandingPage = () => {
           living in <span className="highlight">Tokyo, JP 🇯🇵</span></h1>
           <h3>I'm a self driven and detail-oriented professional with passion and motivation to always keep learning in this ever-evolving sector.</h3>
           <Button onClick={() => window.open(resume)} text={'Check my Resume'} />
-        </div>
-        <div className="landing-image">
-          <img src={require('../../Assets/Images/Landing/landing-bg-1.png').default} alt="Landing Page Image" />
-        </div>
+        </motion.div>
+        <motion.div
+          initial={{
+            opacity: 0.1,
+            scale: 0.8
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1
+          }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="landing-image">
+            <img src={require('../../Assets/Images/Landing/landing-bg-2.svg').default} alt="Landing Page Image" />
+          </div>
+        </motion.div>
       </div>
   )
 }
